e4b9687ce11b5b409b8226a9297ebe222a1f6aaf
infocision <projects@infocision.in>
2020-02-11 15:22:54 +0530

    first commit

--- geo_master/settings.py
+++ geo_master/settings.py
@@ -71,15 +71,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'geo_master.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
--- report/models.py
+++ report/models.py
@@ -1,9 +1,2 @@
 from django.db import models
 
-class Person(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-
-class Address(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
